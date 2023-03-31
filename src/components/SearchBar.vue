77a3a06695ae8bd697d4e1da1a31009ad5af35e2
leonardoserra <serra.leonardo92@gmail.com>
2023-03-31 16:39:28 +0200

    minimo stile headeer

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -11,7 +11,22 @@ export default {
 }
 </script>
 <template>
-    <button @click="$emit('clickSearch')">Cerca</button>
-    <input @keyup.enter="$emit('clickSearch')" type="text" v-model="store.search" placeholder="cerca per nome.." />
+    <div class="my-header">
+        <button @click="$emit('clickSearch')">Cerca</button>
+        <input @keyup.enter="$emit('clickSearch')" type="text" v-model="store.search" placeholder="cerca per nome.." />
+    </div>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.my-header {
+    height: 50px;
+    display: flex;
+    justify-content: end;
+    gap: 5px;
+    padding: 10px 0;
+
+    button,
+    input {
+        padding: 0 5px;
+    }
+}
+</style>
