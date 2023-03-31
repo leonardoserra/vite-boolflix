ddd1f1726293cb098ebdf6060b6413a16cfead8d
leonardoserra <serra.leonardo92@gmail.com>
2023-03-31 15:21:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import { store } from './store.js';
+
+export default {
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+@use "./style/general.scss"
+</style>
