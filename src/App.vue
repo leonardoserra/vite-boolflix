7bae97a985abce29b15f7d79a10d0e797db3cffe
leonardoserra <serra.leonardo92@gmail.com>
2023-03-31 15:38:04 +0200

    faccio chiamata axios ma errore su funzione..

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script>
 import { store } from './store.js';
+import axios from 'axios';
 
 export default {
   data() {
     return {
       store
     }
+  },
+  method: {
+    getMovies() {
+      const endpointRoot = 'https://api.themoviedb.org/3/search/movie?api_key=59d77b0a67470408acdf8c71d519c3b4&language=it-IT&query='
+      axios.get(endpointRoot + 'dragonball')
+        .then(response => {
+          this.store.movies = response.results;
+          console.log(this.store);
+        })
+    }
+  },
+  created() {
+    this.getMovies();
   }
 }
 </script>
