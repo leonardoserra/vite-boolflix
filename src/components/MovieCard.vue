cda0799efe08de2f9a379a9799685b656b2a8276
leonardoserra <serra.leonardo92@gmail.com>
2023-03-31 18:14:32 +0200

    flag dinamiche

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -23,9 +23,14 @@ export default {
             <div class="element">{{ title }}</div>
             <div class="element">{{ og_title }}</div>
             <img v-if="language == 'ja'" class="image" src="../../public/flag_ja.png" alt="flag_japan">
-            <img v-else-if="language == 'en'" class="image" src="/flag_en.png" alt="flag_uk">
-            <img v-else-if="language == 'es'" class="image" src="/flag_es.png" alt="flag_spain">
-            <img v-else-if="language == 'it'" class="image" src="/flag_it.png" alt="flag_italy">
+            <img v-else-if="language == 'en'" class="image" src="../../public/flag_en.png" alt="flag_uk">
+            <img v-else-if="language == 'es'" class="image" src="../../public/flag_es.png" alt="flag_spain">
+            <img v-else-if="language == 'it'" class="image" src="../../public/flag_it.png" alt="flag_italy">
+            <img v-else-if="language == 'fi'" class="image" src="../../public/flag_fi.png" alt="flag_finnish">
+            <img v-else-if="language == 'fr'" class="image" src="../../public/flag_fr.png" alt="flag_france">
+            <img v-else-if="language == 'de'" class="image" src="../../public/flag_de.png" alt="flag_germany">
+            <img v-else-if="language == 'cs'" class="image" src="../../public/flag_cs.png" alt="flag_slovakia">
+            <img v-else-if="language == 'ko'" class="image" src="../../public/flag_ko.png" alt="flag_korea">
             <div v-else class="element">{{ language }}</div>
             <!-- <img :src="getImagePath(this.language)" :alt="language" class="element" /> -->
 
--- src/components/TvSeriesCard.vue
+++ src/components/TvSeriesCard.vue
@@ -15,7 +15,16 @@ export default {
         <div class="card">
             <div class="element">{{ title }}</div>
             <div class="element">{{ og_title }}</div>
-            <div class="element">{{ language }}</div>
+            <img v-if="language == 'ja'" class="image" src="../../public/flag_ja.png" alt="flag_japan">
+            <img v-else-if="language == 'en'" class="image" src="../../public/flag_en.png" alt="flag_uk">
+            <img v-else-if="language == 'es'" class="image" src="../../public/flag_es.png" alt="flag_spain">
+            <img v-else-if="language == 'it'" class="image" src="../../public/flag_it.png" alt="flag_italy">
+            <img v-else-if="language == 'fi'" class="image" src="../../public/flag_fi.png" alt="flag_finnish">
+            <img v-else-if="language == 'fr'" class="image" src="../../public/flag_fr.png" alt="flag_france">
+            <img v-else-if="language == 'de'" class="image" src="../../public/flag_de.png" alt="flag_germany">
+            <img v-else-if="language == 'cs'" class="image" src="../../public/flag_cs.png" alt="flag_slovakia">
+            <img v-else-if="language == 'ko'" class="image" src="../../public/flag_ko.png" alt="flag_korea">
+            <div v-else class="element">{{ language }}</div>
             <div class="element">{{ vote }}</div>
         </div>
 
@@ -33,6 +42,10 @@ export default {
         .element {
             margin: 5px 0;
             background-color: rgb(88, 177, 255);
+        }
+
+        .image {
+            width: 30px;
         }
     }
 }
