870e389311a28d4d9ed7635c1f216554edb5a277
leonardoserra <serra.leonardo92@gmail.com>
2023-03-31 15:57:03 +0200

    creata chiamata per Serie TV

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,26 @@ export default {
       axios.get(endpointRoot + 'dragonball')
         .then(response => {
           this.store.movies = response.data.results;
+          console.log("log di aggiunta Movies:");
+          console.log(this.store);
+        })
+    },
+    getTvSeries() {
+      let endpointRoot = 'https://api.themoviedb.org/3/search/tv?api_key=59d77b0a67470408acdf8c71d519c3b4&language=it-IT&query='
+      axios.get(endpointRoot + 'pokemon')
+        .then(response => {
+          this.store.tvSeries = response.data.results;
+          console.log("log di aggiunta serie TV:");
           console.log(this.store);
         })
     }
   },
   created() {
     this.getMovies();
+    this.getTvSeries();
+    console.log("lo store completo: ");
+    console.log(this.store);
+
   }
 }
 </script>
